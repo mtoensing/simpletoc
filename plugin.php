87daf07cce7afed4484aaab8f10b909bbfe1d970
Marc Tönsing <marc@marc.tv>
2020-04-22 23:24:38 +0200

    Tagging 1.3

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: SimpleTOC - Table of Contents Block
  * Plugin URI: https://github.com/mtoensing/simpletoc
  * Description: Adds a basic "Table of Contents" Gutenberg block.
- * Version: 1.2
+ * Version: 1.3
  * Author: MarcDK
  * Author URI: marc.tv
  * Text Domain: simpletoc
