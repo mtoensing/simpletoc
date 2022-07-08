f98f9f476607ebf7b3449431770d57a7f7590a6a
Marc Toensing <marc.toensing@gmail.com>
2022-07-08 09:46:17 +0200

    update packages

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '18f064aeb57e4c50be2089564b8f5d40');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '3697b39632bfdfd4db58');
