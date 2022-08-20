48862be74578f7f36364f592a618e9e8931d41b1
Marc Toensing <marc.toensing@gmail.com>
2022-08-20 18:48:58 +0200

    build scripts

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '4e1e68ecf2c13a0e2171');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f48138b687fc557f4caa');
