f422b185a2efbea2e6e5c930634a676038de0c62
Marc Toensing <marc.toensing@gmail.com>
2022-11-02 17:06:38 +0100

    tagging 5.0.42

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'b1d7987033d6b506ba47');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'febc8d9f09e493f8f9f6');
