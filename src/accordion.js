f33bb53f4f468dd7f87e73fafcdaffcb60739232
Marc Toensing <marc.toensing@gmail.com>
2022-11-25 13:49:43 +0100

    fixed "let" outside of scope

--- src/accordion.js
+++ src/accordion.js
@@ -1,7 +1,6 @@
 const coll = document.getElementsByClassName( 'simpletoc-collapsible' );
-let i;
 
-for ( i = 0; i < coll.length; i++ ) {
+for ( let i = 0; i < coll.length; i++ ) {
 	coll[ i ].addEventListener( 'click', function () {
 		this.classList.toggle( 'active' );
 		const content = this.nextElementSibling;
