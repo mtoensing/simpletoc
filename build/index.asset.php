41b57e218fe76eaffcecbc17b4f2e8e93070f0ab
Marc Toensing <marc.toensing@gmail.com>
2023-01-06 23:07:23 +0100

    clean up deps

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '10d94e9226b90d623ca7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '782babbeb46d2fcd71aa');
