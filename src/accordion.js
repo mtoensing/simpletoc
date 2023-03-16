9db80772987972c0fbdf758cdbcd93cff4332ddc
Marc Toensing <marc.toensing@gmail.com>
2023-03-16 20:33:40 +0100

    js lint fixes

--- src/accordion.js
+++ src/accordion.js
@@ -1,9 +1,10 @@
-const coll = document.getElementsByClassName('simpletoc-collapsible');
+const coll = document.getElementsByClassName( 'simpletoc-collapsible' );
 
-for (const element of coll) {
-  element.addEventListener('click', function() {
-    this.classList.toggle('active');
-    const content = this.nextElementSibling;
-    content.style.display = content.style.display === 'block' ? 'none' : 'block';
-  });
+for ( const element of coll ) {
+	element.addEventListener( 'click', function () {
+		this.classList.toggle( 'active' );
+		const content = this.nextElementSibling;
+		content.style.display =
+			content.style.display === 'block' ? 'none' : 'block';
+	} );
 }
