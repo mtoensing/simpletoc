b6595768f93e3456f0634b7bf7191377aaffc202
Marc Toensing <marc.toensing@gmail.com>
2023-03-16 14:41:40 +0100

    optimized Accordion JS

--- src/accordion.js
+++ src/accordion.js
@@ -1,13 +1,9 @@
-const coll = document.getElementsByClassName( 'simpletoc-collapsible' );
+const coll = document.getElementsByClassName('simpletoc-collapsible');
 
-for ( let i = 0; i < coll.length; i++ ) {
-	coll[ i ].addEventListener( 'click', function () {
-		this.classList.toggle( 'active' );
-		const content = this.nextElementSibling;
-		if ( content.style.display === 'block' ) {
-			content.style.display = 'none';
-		} else {
-			content.style.display = 'block';
-		}
-	} );
+for (const element of coll) {
+  element.addEventListener('click', function() {
+    this.classList.toggle('active');
+    const content = this.nextElementSibling;
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+  });
 }
