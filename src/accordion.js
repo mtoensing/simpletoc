62a800158549f0e999634d6ebb9c59312e1ce9fc
Marc Toensing <marc.toensing@gmail.com>
2023-09-26 09:27:56 +0200

    lint fix

--- src/accordion.js
+++ src/accordion.js
@@ -1,18 +1,18 @@
-const buttons = document.querySelectorAll( 'button.simpletoc-collapsible' );
+const buttons = document.querySelectorAll('button.simpletoc-collapsible');
 
-buttons.forEach( ( button ) => {
-	button.addEventListener( 'click', function () {
-		this.classList.toggle( 'active' );
+buttons.forEach((button) => {
+	button.addEventListener('click', function () {
+		this.classList.toggle('active');
 		const content = this.parentElement.nextElementSibling;
 		content.style.display =
 			content.style.display === 'block' ? 'none' : 'block';
 
 		// Toggle aria-expanded attribute on the button
-		const ariaExpanded = this.getAttribute( 'aria-expanded' );
-		if ( ariaExpanded === 'true' ) {
-			this.setAttribute( 'aria-expanded', 'false' );
+		const ariaExpanded = this.getAttribute('aria-expanded');
+		if (ariaExpanded === 'true') {
+			this.setAttribute('aria-expanded', 'false');
 		} else {
-			this.setAttribute( 'aria-expanded', 'true' );
+			this.setAttribute('aria-expanded', 'true');
 		}
-	} );
-} );
+	});
+});
--- src/edit.js
+++ src/edit.js
@@ -28,13 +28,13 @@ import { useSelect } from '@wordpress/data';
 import './editor.scss';
 import './accordion.css';
 
-export default function Edit( { attributes, setAttributes } ) {
+export default function Edit({ attributes, setAttributes }) {
 	const blockProps = useBlockProps();
 
 	const { returnisSaving, returnisSavingNonPostEntityChanges } = useSelect(
-		( select ) => {
+		(select) => {
 			const { isSavingPost, isSavingNonPostEntityChanges } =
-				select( editorStore );
+				select(editorStore);
 			return {
 				returnisSaving: isSavingPost(),
 				returnisSavingNonPostEntityChanges:
@@ -47,51 +47,51 @@ export default function Edit( { attributes, setAttributes } ) {
 
 	const controls = (
 		<BlockControls group="block">
-			{ ! ( attributes.no_title || attributes.accordion ) && (
+			{!(attributes.no_title || attributes.accordion) && (
 				<HeadingLevelDropdown
-					selectedLevel={ attributes.title_level }
-					onChange={ ( level ) =>
-						setAttributes( {
-							title_level: Number( level ),
-						} )
+					selectedLevel={attributes.title_level}
+					onChange={(level) =>
+						setAttributes({
+							title_level: Number(level),
+						})
 					}
 				/>
-			) }
+			)}
 			<ToolbarButton
-				icon={ formatListBullets }
-				title={ __( 'Convert to unordered list', 'simpletoc' ) }
-				describedBy={ __( 'Convert to unordered list', 'simpletoc' ) }
-				isActive={ attributes.use_ol === false }
-				onClick={ () => {
-					setAttributes( { use_ol: false } );
-				} }
+				icon={formatListBullets}
+				title={__('Convert to unordered list', 'simpletoc')}
+				describedBy={__('Convert to unordered list', 'simpletoc')}
+				isActive={attributes.use_ol === false}
+				onClick={() => {
+					setAttributes({ use_ol: false });
+				}}
 			/>
 			<ToolbarButton
-				icon={ formatListNumbered }
-				title={ __( 'Convert to ordered list', 'simpletoc' ) }
-				describedBy={ __( 'Convert to ordered list', 'simpletoc' ) }
-				isActive={ attributes.use_ol === true }
-				onClick={ () => {
-					setAttributes( { use_ol: true } );
-				} }
+				icon={formatListNumbered}
+				title={__('Convert to ordered list', 'simpletoc')}
+				describedBy={__('Convert to ordered list', 'simpletoc')}
+				isActive={attributes.use_ol === true}
+				onClick={() => {
+					setAttributes({ use_ol: true });
+				}}
 			/>
 			<ToolbarButton
-				icon={ formatOutdent }
-				title={ __( 'Indent list', 'simpletoc' ) }
-				describedBy={ __( 'Indent list', 'simpletoc' ) }
-				isActive={ attributes.remove_indent === true }
-				onClick={ () => {
-					setAttributes( { remove_indent: true } );
-				} }
+				icon={formatOutdent}
+				title={__('Indent list', 'simpletoc')}
+				describedBy={__('Indent list', 'simpletoc')}
+				isActive={attributes.remove_indent === true}
+				onClick={() => {
+					setAttributes({ remove_indent: true });
+				}}
 			/>
 			<ToolbarButton
-				icon={ formatIndent }
-				title={ __( 'Outdent list', 'simpletoc' ) }
-				describedBy={ __( 'Outdent list', 'simpletoc' ) }
-				isActive={ attributes.remove_indent === false }
-				onClick={ () => {
-					setAttributes( { remove_indent: false } );
-				} }
+				icon={formatIndent}
+				title={__('Outdent list', 'simpletoc')}
+				describedBy={__('Outdent list', 'simpletoc')}
+				isActive={attributes.remove_indent === false}
+				onClick={() => {
+					setAttributes({ remove_indent: false });
+				}}
 			/>
 		</BlockControls>
 	);
@@ -100,142 +100,132 @@ export default function Edit( { attributes, setAttributes } ) {
 		<InspectorControls>
 			<Panel>
 				<PanelBody>
-					{ ! attributes.no_title && (
+					{!attributes.no_title && (
 						<PanelRow>
 							<TextControl
-								label={ __( 'Heading Text', 'simpletoc' ) }
+								label={__('Heading Text', 'simpletoc')}
 								help={
 									__(
 										'Set the heading text of the block.',
 										'simpletoc'
 									) +
 									' ' +
-									__( 'Default value', 'simpletoc' ) +
+									__('Default value', 'simpletoc') +
 									': ' +
-									__( 'Table of Contents', 'simpletoc' )
+									__('Table of Contents', 'simpletoc')
 								}
-								value={ attributes.title_text }
-								onChange={ ( value ) =>
-									setAttributes( {
+								value={attributes.title_text}
+								onChange={(value) =>
+									setAttributes({
 										title_text:
 											value ||
 											__(
 												'Table of Contents',
 												'simpletoc'
 											),
-									} )
+									})
 								}
 							/>
 						</PanelRow>
-					) }
+					)}
 					<PanelRow>
 						<ToggleControl
-							label={ __( 'Remove heading', 'simpletoc' ) }
-							checked={ attributes.no_title }
-							onChange={ () =>
-								setAttributes( {
-									no_title: ! attributes.no_title,
-								} )
+							label={__('Remove heading', 'simpletoc')}
+							checked={attributes.no_title}
+							onChange={() =>
+								setAttributes({
+									no_title: !attributes.no_title,
+								})
 							}
 						/>
 					</PanelRow>
 					<PanelRow>
 						<SelectControl
-							label={ __( 'Minimum level', 'simpletoc' ) }
-							help={ __(
+							label={__('Minimum level', 'simpletoc')}
+							help={__(
 								'Minimum depth of the headings.',
 								'simpletoc'
-							) }
-							value={ attributes.min_level }
-							options={ [
+							)}
+							value={attributes.min_level}
+							options={[
 								{
-									label:
-										__( 'Including', 'simpletoc' ) + ' H6',
+									label: __('Including', 'simpletoc') + ' H6',
 									value: '6',
 								},
 								{
-									label:
-										__( 'Including', 'simpletoc' ) + ' H5',
+									label: __('Including', 'simpletoc') + ' H5',
 									value: '5',
 								},
 								{
-									label:
-										__( 'Including', 'simpletoc' ) + ' H4',
+									label: __('Including', 'simpletoc') + ' H4',
 									value: '4',
 								},
 								{
-									label:
-										__( 'Including', 'simpletoc' ) + ' H3',
+									label: __('Including', 'simpletoc') + ' H3',
 									value: '3',
 								},
 								{
-									label:
-										__( 'Including', 'simpletoc' ) + ' H2',
+									label: __('Including', 'simpletoc') + ' H2',
 									value: '2',
 								},
 								{
 									label:
-										__( 'Including', 'simpletoc' ) +
+										__('Including', 'simpletoc') +
 										' H1 (' +
-										__( 'default', 'simpletoc' ) +
+										__('default', 'simpletoc') +
 										')',
 									value: '1',
 								},
-							] }
-							onChange={ ( level ) =>
-								setAttributes( {
-									min_level: Number( level ),
-								} )
+							]}
+							onChange={(level) =>
+								setAttributes({
+									min_level: Number(level),
+								})
 							}
 						/>
 					</PanelRow>
 					<PanelRow>
 						<SelectControl
-							label={ __( 'Maximum level', 'simpletoc' ) }
-							help={ __(
+							label={__('Maximum level', 'simpletoc')}
+							help={__(
 								'Maximum depth of the headings.',
 								'simpletoc'
-							) }
-							value={ attributes.max_level }
-							options={ [
+							)}
+							value={attributes.max_level}
+							options={[
 								{
 									label:
-										__( 'Including', 'simpletoc' ) +
+										__('Including', 'simpletoc') +
 										' H6 (' +
-										__( 'default', 'simpletoc' ) +
+										__('default', 'simpletoc') +
 										')',
 									value: '6',
 								},
 								{
-									label:
-										__( 'Including', 'simpletoc' ) + ' H5',
+									label: __('Including', 'simpletoc') + ' H5',
 									value: '5',
 								},
 								{
-									label:
-										__( 'Including', 'simpletoc' ) + ' H4',
+									label: __('Including', 'simpletoc') + ' H4',
 									value: '4',
 								},
 								{
-									label:
-										__( 'Including', 'simpletoc' ) + ' H3',
+									label: __('Including', 'simpletoc') + ' H3',
 									value: '3',
 								},
 								{
-									label:
-										__( 'Including', 'simpletoc' ) + ' H2',
+									label: __('Including', 'simpletoc') + ' H2',
 									value: '2',
 								},
 								{
-									label:
-										__( 'Including', 'simpletoc' ) + ' H1',
+									label: __('Including', 'simpletoc') + ' H1',
 									value: '1',
 								},
-							] }
-							onChange={ ( level ) =>
-								setAttributes( {
-									max_level: Number( level ),
-								} )
+							]}
+							onChange={(level) =>
+								setAttributes({
+									max_level: Number(level),
+								})
 							}
 						/>
 					</PanelRow>
@@ -243,96 +233,90 @@ export default function Edit( { attributes, setAttributes } ) {
 			</Panel>
 			<Panel>
 				<PanelBody
-					title={ __( 'Advanced Features', 'simpletoc' ) }
-					icon={ advpanelicon }
-					initialOpen={ false }
+					title={__('Advanced Features', 'simpletoc')}
+					icon={advpanelicon}
+					initialOpen={false}
 				>
 					<PanelRow>
 						<div
-							style={ {
+							style={{
 								marginBottom: '1em',
 								border: '1px solid rgba(0, 0, 0, 0.05)',
 								padding: '0.5em',
 								backgroundColor: '#f7f7f7',
-							} }
+							}}
 						>
 							<p>
 								<strong>
-									{ __(
+									{__(
 										'Think about making a donation if you use any of these features.',
 										'simpletoc'
-									) }
+									)}
 								</strong>
 							</p>
 							<ExternalLink href="https://marc.tv/out/donate">
-								{ __( 'Donate here!', 'simpletoc' ) }
+								{__('Donate here!', 'simpletoc')}
 							</ExternalLink>
 						</div>
 					</PanelRow>
 					<PanelRow>
 						<ToggleControl
-							label={ __(
-								'Hide in accordion menu',
-								'simpletoc'
-							) }
-							help={ __(
+							label={__('Hide in accordion menu', 'simpletoc')}
+							help={__(
 								'Adds minimal JavaScript and css styles.',
 								'simpletoc'
-							) }
-							checked={ attributes.accordion }
-							onChange={ () =>
-								setAttributes( {
-									accordion: ! attributes.accordion,
-								} )
+							)}
+							checked={attributes.accordion}
+							onChange={() =>
+								setAttributes({
+									accordion: !attributes.accordion,
+								})
 							}
 						/>
 					</PanelRow>
 					<PanelRow>
 						<ToggleControl
-							label={ __(
-								'Smooth scrolling support',
-								'simpletoc'
-							) }
-							help={ __(
+							label={__('Smooth scrolling support', 'simpletoc')}
+							help={__(
 								'Adds the following CSS to the HTML element: "scroll-behavior: smooth;"',
 								'simpletoc'
-							) }
-							checked={ attributes.add_smooth }
-							onChange={ () =>
-								setAttributes( {
-									add_smooth: ! attributes.add_smooth,
-								} )
+							)}
+							checked={attributes.add_smooth}
+							onChange={() =>
+								setAttributes({
+									add_smooth: !attributes.add_smooth,
+								})
 							}
 						/>
 					</PanelRow>
 					<PanelRow>
 						<ToggleControl
-							label={ __( 'Use absolute urls', 'simpletoc' ) }
-							help={ __(
+							label={__('Use absolute urls', 'simpletoc')}
+							help={__(
 								'Adds the permalink url to the fragment.',
 								'simpletoc'
-							) }
-							checked={ attributes.use_absolute_urls }
-							onChange={ () =>
-								setAttributes( {
+							)}
+							checked={attributes.use_absolute_urls}
+							onChange={() =>
+								setAttributes({
 									use_absolute_urls:
-										! attributes.use_absolute_urls,
-								} )
+										!attributes.use_absolute_urls,
+								})
 							}
 						/>
 					</PanelRow>
 					<PanelRow>
 						<ToggleControl
-							label={ __( 'Wrapper div', 'simpletoc' ) }
-							help={ __(
+							label={__('Wrapper div', 'simpletoc')}
+							help={__(
 								'Additionally adds the role "navigation" and ARIA attributes.',
 								'simpletoc'
-							) }
-							checked={ attributes.wrapper }
-							onChange={ () =>
-								setAttributes( {
-									wrapper: ! attributes.wrapper,
-								} )
+							)}
+							checked={attributes.wrapper}
+							onChange={() =>
+								setAttributes({
+									wrapper: !attributes.wrapper,
+								})
 							}
 						/>
 					</PanelRow>
@@ -342,17 +326,17 @@ export default function Edit( { attributes, setAttributes } ) {
 	);
 
 	return (
-		<div { ...blockProps }>
-			{ controls }
-			{ controlssidebar }
-			{ returnisSaving || returnisSavingNonPostEntityChanges ? (
+		<div {...blockProps}>
+			{controls}
+			{controlssidebar}
+			{returnisSaving || returnisSavingNonPostEntityChanges ? (
 				<Spinner />
 			) : (
 				<ServerSideRender
 					block="simpletoc/toc"
-					attributes={ attributes }
+					attributes={attributes}
 				/>
-			) }
+			)}
 		</div>
 	);
 }
