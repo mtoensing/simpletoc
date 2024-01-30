75a0253532533646731df8b374ae6865e4ca87a0
Marc Toensing <marc.toensing@gmail.com>
2024-01-30 13:00:12 +0100

    linted js

--- src/accordion.js
+++ src/accordion.js
@@ -1,14 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('button.simpletoc-collapsible');
+document.addEventListener( 'DOMContentLoaded', () => {
+	const buttons = document.querySelectorAll( 'button.simpletoc-collapsible' );
 
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            button.classList.toggle('active');
-            const content = button.parentElement.nextElementSibling;
-            const isCollapsed = !content.style.maxHeight || content.style.maxHeight === '0px';
+	buttons.forEach( ( button ) => {
+		button.addEventListener( 'click', () => {
+			button.classList.toggle( 'active' );
+			const content = button.parentElement.nextElementSibling;
+			const isCollapsed =
+				! content.style.maxHeight || content.style.maxHeight === '0px';
 
-            button.setAttribute('aria-expanded', isCollapsed ? 'true' : 'false');
-            content.style.maxHeight = isCollapsed ? `${content.scrollHeight}px` : '0px';
-        });
-    });
-});
+			button.setAttribute(
+				'aria-expanded',
+				isCollapsed ? 'true' : 'false'
+			);
+			content.style.maxHeight = isCollapsed
+				? `${ content.scrollHeight }px`
+				: '0px';
+		} );
+	} );
+} );
