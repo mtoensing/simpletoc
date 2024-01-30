8e5627e40b814b52d836c4c659de1fe482e7b14e
Marc Toensing <marc.toensing@gmail.com>
2024-01-30 12:33:24 +0100

    linted js

--- src/accordion.js
+++ src/accordion.js
@@ -1,27 +1,26 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const buttons = document.querySelectorAll('button.simpletoc-collapsible');
+document.addEventListener( 'DOMContentLoaded', () => {
+	const buttons = document.querySelectorAll( 'button.simpletoc-collapsible' );
 
-    buttons.forEach((button) => {
-        button.addEventListener('click', function () {
-			this.classList.toggle('active');
-            const content = this.parentElement.nextElementSibling;
-            const isCollapsed = !content.style.maxHeight || content.style.maxHeight === '0px';
+	buttons.forEach( ( button ) => {
+		button.addEventListener( 'click', function () {
+			this.classList.toggle( 'active' );
+			const content = this.parentElement.nextElementSibling;
+			const isCollapsed =
+				! content.style.maxHeight || content.style.maxHeight === '0px';
 			const ariaExpanded = this.getAttribute( 'aria-expanded' );
 			if ( ariaExpanded === 'true' ) {
 				this.setAttribute( 'aria-expanded', 'false' );
 			} else {
 				this.setAttribute( 'aria-expanded', 'true' );
 			}
-			
 
-
-            if (isCollapsed) {
-                // Expand the content
-                content.style.maxHeight = content.scrollHeight + 'px';
-            } else {
-                // Collapse the content
-                content.style.maxHeight = '0px';
-            }
-        });
-    });
-});
+			if ( isCollapsed ) {
+				// Expand the content
+				content.style.maxHeight = content.scrollHeight + 'px';
+			} else {
+				// Collapse the content
+				content.style.maxHeight = '0px';
+			}
+		} );
+	} );
+} );
