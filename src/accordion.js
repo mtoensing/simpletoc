879f29c602dcdebde0f167fa5a663e796a3acdd8
Marc Toensing <marc.toensing@gmail.com>
2024-01-30 12:59:45 +0100

    6.2 with better JS code

--- src/accordion.js
+++ src/accordion.js
@@ -1,26 +1,14 @@
-document.addEventListener( 'DOMContentLoaded', () => {
-	const buttons = document.querySelectorAll( 'button.simpletoc-collapsible' );
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('button.simpletoc-collapsible');
 
-	buttons.forEach( ( button ) => {
-		button.addEventListener( 'click', function () {
-			this.classList.toggle( 'active' );
-			const content = this.parentElement.nextElementSibling;
-			const isCollapsed =
-				! content.style.maxHeight || content.style.maxHeight === '0px';
-			const ariaExpanded = this.getAttribute( 'aria-expanded' );
-			if ( ariaExpanded === 'true' ) {
-				this.setAttribute( 'aria-expanded', 'false' );
-			} else {
-				this.setAttribute( 'aria-expanded', 'true' );
-			}
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            button.classList.toggle('active');
+            const content = button.parentElement.nextElementSibling;
+            const isCollapsed = !content.style.maxHeight || content.style.maxHeight === '0px';
 
-			if ( isCollapsed ) {
-				// Expand the content
-				content.style.maxHeight = content.scrollHeight + 'px';
-			} else {
-				// Collapse the content
-				content.style.maxHeight = '0px';
-			}
-		} );
-	} );
-} );
+            button.setAttribute('aria-expanded', isCollapsed ? 'true' : 'false');
+            content.style.maxHeight = isCollapsed ? `${content.scrollHeight}px` : '0px';
+        });
+    });
+});
