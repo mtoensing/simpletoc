2901ac79b114ae28814280e18f253f16bb7a49a0
Marc Toensing <marc.toensing@gmail.com>
2024-03-11 22:31:53 +0100

    wp-scripts plugin-zip

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '6a8ed27720061482c8e3');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '5515d55dd32682e0483d');
