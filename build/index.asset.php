b4517753ebc6d30243a996b7048c36cb8a42a58a
Marc Toensing <marc.toensing@gmail.com>
2024-03-11 22:28:58 +0100

    added plugin-zip

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'd8d5b1bfb84a5962113f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '6a8ed27720061482c8e3');
