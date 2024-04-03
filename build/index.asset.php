b092d3269a4ebfb2c73e8d1dd1bc1971c1910ffe
Marc Toensing <marc.toensing@gmail.com>
2024-04-03 20:02:12 +0200

    v 6.4

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '9a4b363fa03050a7f329');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'c2d1e8b991ac1f7111cd');
