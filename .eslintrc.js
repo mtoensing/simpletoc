44c39d6e937e972411bc9d4c7153d5d99b9e7879
Marc Toensing <marc.toensing@gmail.com>
2025-12-30 23:02:32 +0100

    Reformat ESLint config with tabs for indentation
    Changed indentation in .eslintrc.js from spaces to tabs for consistency with project style. No functional changes to ESLint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
-  rules: {
-    // WordPress provides these as externals
-    'import/no-unresolved': [
-      'error',
-      {
-        ignore: [ '^@wordpress/' ],
-      },
-    ],
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
+	rules: {
+		// WordPress provides these as externals
+		'import/no-unresolved': [
+			'error',
+			{
+				ignore: [ '^@wordpress/' ],
+			},
+		],
 
-    // Do NOT require @wordpress/* to be installed
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        optionalDependencies: [ '@wordpress/*' ],
-      },
-    ],
-  },
+		// Do NOT require @wordpress/* to be installed
+		'import/no-extraneous-dependencies': [
+			'error',
+			{
+				optionalDependencies: [ '@wordpress/*' ],
+			},
+		],
+	},
 };
